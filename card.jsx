908249e633706cfa16311688de397577273a94b8
Giridhar Raj <95267313+Giridharaj@users.noreply.github.com>
2025-09-01 23:58:44 +0530

    Create card.jsx

---
+++ card.jsx
@@ -0,0 +1,9 @@
+function card(){
+    return (
+        <div className="card">
+          <img src="file:///C:/Users/HP/OneDrive/%E7%94%BB%E5%83%8F/GW-Generated-Image-8_22_2025__8_46_55_PM.png" alt="profile picture"></img>
+          <h2>Virtual Reality</h2>
+          <p>Just a virtual imagination of visualizing the unleashed things</p>
+        </div>
+    );
+}
