3cd574af46a31d283e5e45dd1d83d1f7c98ef50e
Giridhar Raj <95267313+Giridharaj@users.noreply.github.com>
2025-09-02 23:18:43 +0530

    Create button.jsx

---
+++ button.jsx
@@ -0,0 +1,13 @@
+function button(){
+    const styles={
+        backgroundColor: 'blue',
+        color:'white',
+        padding:"10px 20px",
+        borderRadius:"5px",
+        border: "none",
+        cursor: "pointer",
+    }
+    return(<button style={styles}>click me</button>)
+}
+
+export default button;
